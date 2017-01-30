355f7e275f34661c3b8eeb81edf8b1931b3c3d6c
weng007 <awoot.r@gmail.com>
2017-01-30 12:13:38 +0700

    w

---
+++ AlphaApi/AlphaApi/Controllers/QuotationController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AlphaApi.DataAccessLayer;
+using Newtonsoft.Json;
+using System.Web.Http.Cors;
+
+namespace AlphaApi.Controllers
+{
+    [EnableCorsAttribute("*", "*", "*")]
+    public class QuotationController : ApiController
+    {
+        static QuotationDAL quotationdb = new QuotationDAL();
+
+        [HttpGet]
+        public string Get()
+        {
+            var response = quotationdb.SelectData();
+            return JsonConvert.SerializeObject(response, Formatting.Indented);
+        }
+    }
+}
