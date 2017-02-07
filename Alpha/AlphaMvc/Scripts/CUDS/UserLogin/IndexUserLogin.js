ce7a8ab47234886a6203dec37fd59b587619f6d9
pipattra <pipattrakum@gmail.com>
2017-02-07 12:36:03 +0700

    b

--- Alpha/AlphaMvc/Scripts/CUDS/UserLogin/IndexUserLogin.js
+++ Alpha/AlphaMvc/Scripts/CUDS/UserLogin/IndexUserLogin.js
@@ -1,47 +1,45 @@
-//$(document).ready(function () {
-
-//    $("#UserLogin").click(function () {
-//        alert("test0");
-        
-//    });
-//});
-
-function GetData() {
-    alert("test0");
-    var dataObject = { UserName: $("#txtUserName").val(), Password: $("#txtpassword").val() };
-    //console.log(dataObject);
-    var IsExists = false;
-    $.ajax(
-    {
-        url: 'http://localhost:13131/api/UserLogin',
-        type: 'GET',
-        async: false,
-        datatype: 'json',
-        success: function (data) {
-            data = JSON.parse(data);
-            console.log(data);
-            for (var i = 0; i < data.Table.length; i++) {
-                if (dataObject.UserName == data.Table[i].UserName) {
-                    console.log(dataObject.UserName);
-                    IsExists = true;
+$(document).ready(function () {
+    $("#UserLogin").click(function () {
+        alert("test0");
+        var dataObject = { UserName: $("#txtUserName").val(), Password: $("#txtpassword").val() };
+        //console.log(dataObject);
+        var IsExists = false;
+        var html = '<body>';
+        $.ajax(
+        {
+            url: 'http://localhost:13131/api/UserLogin',
+            type: 'GET',
+            async: false,
+            datatype: 'json',
+            success: function (data) {
+                data = JSON.parse(data);
+                console.log(data);
+                for (var i = 0; i < data.Table.length; i++) {
+                    if (dataObject.UserName == data.Table[i].UserName) {
+                        console.log(dataObject.UserName);
+                        IsExists = true;
+                        var val = IsExists;
+                    }
                 }
-            }
 
-            if (IsExists) {
-                alert('Insert');
-                alert(IsExists);
-                document.getElementById("demo").innerHTML = IsExists;
-                //window.location = 'http://localhost:1042/Technician/indexTechnician';
-                //window.location = "/Home/Index";
-            }
-            else {
-                alert('User is not found');
-            }
+                if (IsExists) {
+                    alert('Insert');
+                }
+                else {
+                    alert('User is not found');
+                }
 
-
-        },
-        error: function (msg) {
-            alert(msg)
-        }
+                document.getElementById("result").innerHTML = html;
+                alert(html);
+            },
+            error: function (msg) {
+                alert(msg)
+            }
+        });
     });
-}
+});
+
+//function init()
+//{
+//    window.location = "http://localhost:1042/Technician/indexTechnician";        
+//}
