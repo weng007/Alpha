558b982c825a3afde97a839a67ab70d5c1c5d81c
weng007 <awoot.r@gmail.com>
2017-04-04 21:05:26 +0700

    w

--- Alpha/AlphaMvc/DataAccessLayer/DBdata.cs
+++ Alpha/AlphaMvc/DataAccessLayer/DBdata.cs
@@ -12,37 +12,31 @@ namespace AlphaMVC.DataAccessLayer
     public class DBdata
     {
         string conStr = ConfigurationManager.ConnectionStrings["mycon"].ConnectionString;
-            //public string InsertData(TechnicianModels MD)
-            //{
-            //    SqlConnection con = null;
-            //    string result = "";
-            //    using (SqlConnection conObj = new SqlConnection(conStr))
-            //    {
-            //        try
-            //        {
-            //                SqlCommand cmd = new SqlCommand("SP_Mobile_Ins", conObj);
-            //                cmd.CommandType = CommandType.StoredProcedure;
-            //                cmd.Parameters.AddWithValue("@MobileName", MD.MobileName);
-            //                cmd.Parameters.AddWithValue("@MobileIMEno", MD.MobileIMEno);
-            //                cmd.Parameters.AddWithValue("@mobileprice", MD.mobileprice);
-            //                cmd.Parameters.AddWithValue("@mobileManufacured", MD.mobileManufacured);
-            //                conObj.Open();
-            //                result = cmd.ExecuteScalar().ToString();
+        public string GetRptJobOrder(string jobID)
+        {
+            string result = "";
+            using (SqlConnection conObj = new SqlConnection(conStr))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("SP_Mobile_Ins", conObj);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@JobID", jobID);
+                    conObj.Open();
+                    result = cmd.ExecuteScalar().ToString();
 
-            //                return result;
+                    return result;
 
-            //        }
-            //        catch
-            //        {
-            //            return result = "";
-            //        }
-            //        finally
-            //        {
-            //            conObj.Close();
-            //        }
-            //    }
-            //}
-        //}
-
+                }
+                catch
+                {
+                    return result = "";
+                }
+                finally
+                {
+                    conObj.Close();
+                }
+            }
+        }
     }
 }
