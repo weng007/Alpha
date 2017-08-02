5eaf24e4c7902987aa3f123f9e8032c6bd37d71c
pipattra <pipattrakum@gmail.com>
2017-08-02 12:23:08 +0700

    b

--- Alpha/AlphaMvc/Scripts/CUDS/IncomeMaster/CreateIncomeMaster.js
+++ Alpha/AlphaMvc/Scripts/CUDS/IncomeMaster/CreateIncomeMaster.js
@@ -1,6 +1,6 @@
 function CreateData() {
     var Price = $("#txtPricelist").val().replace(',', '');
-    var dataObject = { Detail: $("#txtDetail").val(), PriceList: Price, Seq: $("#txtSqe").val(), CreateBy: localStorage['UserID'], EditBy: localStorage['UserID'] };
+    var dataObject = { Detail: $("#txtDetail").val(), PriceList: Price, Seq: $("#txtSeq").val(), CreateBy: localStorage['UserID'], EditBy: localStorage['UserID'] };
     $.ajax(
     {
         url: 'http://localhost:13131/api/IncomeMaster',
@@ -19,6 +19,6 @@ function CreateData() {
     });
 }
 function convertFloat(str) {
-
+    //alert("Test");
     $(str).val($(str).val().replace(',', '')).formatNumber({ format: "#,###.00", locale: "us" });
 }
